ef967ddca7f4e605ad025f617a8d0fd892d1288c
David Ra Youssef <davidradeveloper@gmail.com>
2016-11-20 13:28:05 -0500

    Add data array

---
+++ src/data/data.js
@@ -0,0 +1,296 @@
+export const data = [
+  {
+    "name": "Youtube Channels",
+    "icon": "",
+    "heading": "Watch...",
+    "links": [
+      {
+        "name": "Wes Bos",
+        "thumbnail": "https://yt3.ggpht.com/-E0Ej_rdX5Ic/AAAAAAAAAAI/AAAAAAAAAAA/kskO1deXSNs/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "url": "https://www.youtube.com/user/wesbos/videos"
+      },
+      {
+        "name": "funfunfunction",
+        "thumbnail": "https://yt3.ggpht.com/-5ny40r8qe90/AAAAAAAAAAI/AAAAAAAAAAA/IrloZ_OeiYc/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "url": "https://www.youtube.com/channel/UCO1cgjhGzsSYb1rsB4bFe4Q/videos"
+      },
+      {
+        "name": "LearnCode.academy",
+        "thumbnail": "https://yt3.ggpht.com/-mW8hwFBRUNs/AAAAAAAAAAI/AAAAAAAAAAA/jVL9TZ64o7A/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "url": "https://www.youtube.com/user/learncodeacademy/videos"
+      },
+      {
+        "name": "LevelUpTuts",
+        "thumbnail": "https://yt3.ggpht.com/-MogFBYbYHgM/AAAAAAAAAAI/AAAAAAAAAAA/8e-NkR3Q1LE/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "url": "https://www.youtube.com/user/LevelUpTuts/videos"
+      },
+      {
+        "name": "Bocoup LLC",
+        "thumbnail": "https://i.ytimg.com/i/QUPI1PxfE4-bqwuI26I2HA/mq1.jpg?v=51103bcb",
+        "url": "https://www.youtube.com/user/BocoupLLC/videos"
+      },
+      {
+        "name": "CSS-Tricks",
+        "thumbnail": "https://yt3.ggpht.com/-h7mfjiwQFZo/AAAAAAAAAAI/AAAAAAAAAAA/f0Iw26gcn7w/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "url": "https://www.youtube.com/user/realcsstricks/videos"
+      },
+      {
+        "name": "ReactCasts",
+        "thumbnail": "https://yt3.ggpht.com/-KA7RKBT2PHk/AAAAAAAAAAI/AAAAAAAAAAA/hsAkoB2JT4U/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "url": "https://www.youtube.com/channel/UCZkjWyyLvzWeoVWEpRemrDQ/videos"
+      },
+      {
+        "name": "readthesource",
+        "thumbnail": "https://yt3.ggpht.com/-5pcsTtjX9lQ/AAAAAAAAAAI/AAAAAAAAAAA/386MrHky1_0/s100-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "url": "https://www.youtube.com/user/readthesource/videos"
+      }
+    ]
+  },
+  {
+    "name": "Podcasts",
+    "icon": "",
+    "heading": "Listen to...",
+    "links": [
+      {
+        "name": "JavaScript Jabber",
+        "thumbnail": null,
+        "url": "https://devchat.tv/js-jabber"
+      },
+      {
+        "name": "Shop Talk Show",
+        "thumbnail": "http://shoptalkshow.com/wp-content/uploads/2012/01/000-Nobody-125x125.jpg",
+        "url": "http://shoptalkshow.com"
+      },
+      {
+        "name": "Adventures in Angular",
+        "thumbnail": null,
+        "url": "https://devchat.tv/adv-in-angular"
+      }
+    ]
+  },
+  {
+    "name": "Reddit Subreddits",
+    "icon": "",
+    "heading": "Subscribe to...",
+    "links": [
+      {
+        "name": "/r/angularjs",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/angularjs/"
+      },
+      {
+        "name": "/r/frontend",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/frontend/"
+      },
+      {
+        "name": "/r/javascript",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/javascript/"
+      },
+      {
+        "name": "/r/learnjavascript",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/learnjavascript/"
+      },
+      {
+        "name": "/r/learnprogramming",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/learnprogramming/"
+      },
+      {
+        "name": "/r/programming",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/programming/"
+      },
+      {
+        "name": "/r/reactjs",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/reactjs/"
+      },
+      {
+        "name": "/r/web_design",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/web_design/"
+      },
+      {
+        "name": "/r/webdev",
+        "thumbnail": null,
+        "url": "https://www.reddit.com/r/webdev/"
+      }
+    ]
+  },
+  {
+    "name": "Twitter Accounts",
+    "icon": "",
+    "heading": "Follow...",
+    "links": [
+      {
+        "name": "Dan Abramov",
+        "thumbnail": "https://pbs.twimg.com/profile_images/553711083064541184/9VsY9i09_bigger.jpeg",
+        "url": "https://twitter.com/dan_abramov"
+      },
+      {
+        "name": "Addy Osmani",
+        "thumbnail": "https://pbs.twimg.com/profile_images/725034709533728770/ZVaJZ_tu_bigger.jpg",
+        "url": "https://twitter.com/addyosmani"
+      },
+      {
+        "name": "Paul Irish",
+        "thumbnail": "https://pbs.twimg.com/profile_images/420826194083213312/CP1RmLa3_bigger.jpeg",
+        "url": "https://twitter.com/paul_irish"
+      },
+      {
+        "name": "Echo JS",
+        "thumbnail": "https://pbs.twimg.com/profile_images/3225992358/d40ef3d81a730788f7da4e1649fddeef_bigger.png",
+        "url": "https://twitter.com/echojs"
+      },
+      {
+        "name": "JavaScript Daily",
+        "thumbnail": "https://pbs.twimg.com/profile_images/710428065369890816/ZaHBODSJ_bigger.jpg",
+        "url": "https://twitter.com/JavaScriptDaily"
+      },
+      {
+        "name": "Front-End Daily",
+        "thumbnail": "https://pbs.twimg.com/profile_images/719462261375561729/_WOtWeTK_bigger.jpg",
+        "url": "https://twitter.com/FrontEndDaily"
+      },
+      {
+        "name": "Chrome DevTools",
+        "thumbnail": "https://pbs.twimg.com/profile_images/762579160099385344/w5MfsnMn_bigger.jpg",
+        "url": "https://twitter.com/ChromeDevTools"
+      },
+      {
+        "name": "Front-End Front",
+        "thumbnail": "https://pbs.twimg.com/profile_images/539519250545668096/2hF95nBY_bigger.png",
+        "url": "https://twitter.com/frontendfront"
+      },
+      {
+        "name": "CSS-Tricks",
+        "thumbnail": "https://pbs.twimg.com/profile_images/546011298627334146/MAeF2-yK_bigger.jpeg",
+        "url": "https://twitter.com/Real_CSS_Tricks"
+      },
+      {
+        "name": "Eric Elliot",
+        "thumbnail": "https://pbs.twimg.com/profile_images/464310668984725504/ym-M-SNv_bigger.jpeg",
+        "url": "https://twitter.com/_ericelliott"
+      },
+      {
+        "name": "Paul Lewis",
+        "thumbnail": "https://pbs.twimg.com/profile_images/692819773147652096/sHmbiKlK_bigger.jpg",
+        "url": "https://twitter.com/aerotwist"
+      },
+      {
+        "name": "Wes Bos",
+        "thumbnail": "https://pbs.twimg.com/profile_images/623184294521929728/LUJ4qL8n_bigger.jpg",
+        "url": "https://twitter.com/wesbos"
+      },
+      {
+        "name": "Umar Hansa",
+        "thumbnail": "https://pbs.twimg.com/profile_images/506129464405663744/kUMAxdaf_bigger.jpeg",
+        "url": "https://twitter.com/umaar"
+      }
+    ]
+  },
+  {
+    "name": "Email newsletters",
+    "icon": "",
+    "heading": "Sign up to...",
+    "links": [
+      {
+        "name": "JavaScript Weekly",
+        "thumbnail": null,
+        "url": "http://javascriptweekly.com/"
+      },
+      {
+        "name": "Front End Focus",
+        "thumbnail": null,
+        "url": "http://frontendfocus.co/"
+      },
+      {
+        "name": "Mobile Web Weekly",
+        "thumbnail": null,
+        "url": "http://mobilewebweekly.co/"
+      },
+      {
+        "name": "React Status",
+        "thumbnail": null,
+        "url": "http://react.statuscode.com/latest"
+      },
+      {
+        "name": "This Week in React",
+        "thumbnail": null,
+        "url": "https://thisweekinreact.com/latest"
+      },
+      {
+        "name": "Node Weekly",
+        "thumbnail": null,
+        "url": "http://nodeweekly.com/"
+      },
+      {
+        "name": "ng-newsletter",
+        "thumbnail": null,
+        "url": "http://cur.ng-newsletter.com/"
+      },
+      {
+        "name": "React Newsletter",
+        "thumbnail": null,
+        "url": "http://reactjsnewsletter.com/"
+      }
+    ]
+  },
+  {
+    "name": "Code Courses",
+    "icon": "",
+    "heading": "Learn at...",
+    "links": [
+      {
+        "name": "Code School",
+        "thumbnail": "https://www.codeschool.com/assets/pages/brand/downloads/brand-mark-47100518b9ef3c7d41cb081576451d74f8ce24b4cc6f7948cb6913f84da3072d.png",
+        "url": "https://www.codeschool.com/"
+      },
+      {
+        "name": "Pluralsight",
+        "thumbnail": null,
+        "url": "https://www.pluralsight.com/"
+      },
+      {
+        "name": "egghead.io",
+        "thumbnail": "https://d1xwtr0qwr70yv.cloudfront.net/assets/elements/logo-mobile-0012236aa1a9766db6b0cc9705df19ec.svg",
+        "url": "https://egghead.io/courses"
+      },
+      {
+        "name": "Front End Masters",
+        "thumbnail": "https://frontendmasters.com/wp-content/themes/frontendmasters/assets/v1.1/images/workshop/logo-frontendmasters.png",
+        "url": "https://frontendmasters.com/"
+      }
+    ]
+  },
+  {
+    "name": "Coding Challenges",
+    "icon": "",
+    "heading": "Practice at...",
+    "links": [
+      {
+        "name": "Codewars",
+        "thumbnail": "https://www.codewars.com/assets/logos/logo-square-red-big-dc2f35502ee2b60882a41327c529521c.png",
+        "url": "http://www.codewars.com/"
+      },
+      {
+        "name": "Hackerrank",
+        "thumbnail": "http://blog.hackerrank.com/wp-content/uploads/2016/03/logo_icon_1501.png",
+        "url": "https://www.hackerrank.com/"
+      },
+      {
+        "name": "freeCodeCamp",
+        "thumbnail": "",
+        "url": "https://www.freecodecamp.com/map"
+      },
+      {
+        "name": "leetcode",
+        "thumbnail": null,
+        "url": "https://leetcode.com"
+      }
+    ]
+  }
+];
