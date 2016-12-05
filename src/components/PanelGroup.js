e4132aa57450ce275b3623c0e78037f5bed3aa26
David Ra Youssef <davidradeveloper@gmail.com>
2016-12-05 11:56:10 -0500

    Refactor to add in rows for masonry layout effect

--- src/components/PanelGroup.js
+++ src/components/PanelGroup.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import Panel from './Panel';
 
-const PanelGroup = ({data}) => {
+const PanelGroup = ({ data }) => {
+  // In order to get the Masonry effect, we need to wrap every 3 panels in divs
+  let panels = data
+    .map(panel => {
+      return (
+        <Panel
+          key={panel.name}
+          heading={panel.heading}
+          name={panel.name}
+          links={panel.links}
+        />
+      );
+    }) // First we wrap each panel in an li tag
+    .map((v,i,a) => {
+      if (i % 3 === 0) {
+        return (
+          <div key={i} className="column">{a.slice(i, i + 3)}</div>
+        ); // Then wrap every 3 lis in a div
+      }
+    })
+    .filter(v => v !== undefined); // Then remove undefined values returned because of above condition
+
   return (
     <ul className="panel-group">
-      {data.map(panel => {
-        return (
-          <Panel
-            key={panel.name}
-            heading={panel.heading}
-            name={panel.name}
-            links={panel.links}
-          />
-        );
-      })}
+      {panels}
     </ul>
   );
 };
