ada3774ad824eb529789c9b66bb397d44e437174
David Ra Youssef <davidradeveloper@gmail.com>
2016-12-08 13:48:19 -0500

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header-main">
+      <h1>devLinks</h1>
+      <h2>Resources for Front-End Developers</h2>
+      <div>Curated by D.R.</div>
+    </header>
+  );
+};
+
+export default Header;
