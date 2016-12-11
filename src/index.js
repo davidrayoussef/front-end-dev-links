c61da4c01e557efa6283c6627676a54c3b271caa
David Ra Youssef <davidradeveloper@gmail.com>
2016-12-11 13:23:15 -0500

    Configure react-router and add Routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import PanelGroup from './components/PanelGroup';
+import Grid from './components/Grid';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import './style.css';
+
+const App = () => {
+  return (
+    <Router history={browserHistory}>
+      <Route path="/" component={PanelGroup}>
+        <IndexRoute component={PanelGroup} />
+      </Route>
+      <Route path={"grid"} component={Grid} />
+    </Router>
+  );
+};
 
 render(
   <App />, document.getElementById('root')
