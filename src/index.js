e7a241b56e649443de80a43771b275905dbe32a7
David Ra Youssef <davidradeveloper@gmail.com>
2016-12-22 13:34:42 -0500

    Change browserHistory to hashHistory to fix route handler breaking on refresh

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import PanelGroup from './components/PanelGroup';
 import Grid from './components/Grid';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import './style.css';
 
 const rootPath = process.env.ENV === 'production' ? '/front-end-dev-links' : '/';
 
 const App = () => {
   return (
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path={rootPath} component={PanelGroup}>
         <IndexRoute component={PanelGroup} />
       </Route>
