23e1cf9fac8cd2b96bc27b8b8b2c5200c5620b82
David Ra Youssef <davidradeveloper@gmail.com>
2016-12-26 11:39:45 -0500

    Fix share links

--- src/components/Share.js
+++ src/components/Share.js
@@ -9,22 +9,34 @@ const Share = () => {
   return (
     <div className="share">
 
-      <a href="https://twitter.com/intent/tweet/?text=Front-End Dev Links - Resources for Front-End Developers&amp;url=http://davidra.co/front-end-dev-links/" target="_blank">
-        <svg className="twitter" x="0px" y="0px" width="60px" height="60px" viewBox="0 0 60 60">
+      <a
+        className="twitter"
+        href="https://twitter.com/intent/tweet/?text=Front-End%20Dev%20Links%20-%20Resources%20for%20Front-End%20Developers&amp;url=http://davidra.co/front-end-dev-links/"
+        target="_blank"
+      >
+        <svg x="0px" y="0px" width="60px" height="60px" viewBox="0 0 60 60">
           <TwitterIcon />
         </svg>
         <span>Tweet</span>
       </a>
 
-      <a target="_blank">
-        <svg className="facebook" x="0px" y="0px" width="60px" height="60px" viewBox="0 0 60 60">
+      <a
+        className="facebook"
+        href="https://www.facebook.com/sharer/sharer.php?u=http%3A//davidra.co/front-end-dev-links/&name=Front-End%20Dev%20Links&description=Resources%20for%20Front-End%20Developers"
+        target="_blank"
+      >
+        <svg x="0px" y="0px" width="60px" height="60px" viewBox="0 0 60 60">
           <FacebookIcon />
         </svg>
         <span>Share</span>
       </a>
 
-      <a target="_blank">
-        <svg className="github" x="0px" y="0px" width="60px" height="60px" viewBox="0 0 60 60">
+      <a
+        className="github"
+        href="https://github.com/davidrayoussef/front-end-dev-links"
+        target="_blank"
+      >
+        <svg x="0px" y="0px" width="60px" height="60px" viewBox="0 0 60 60">
           <GithubIcon />
         </svg>
         <span>Star</span>
