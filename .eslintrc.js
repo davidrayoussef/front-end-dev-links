7802efac37671c29625969712852c0e34803588b
David Ra Youssef <davidradeveloper@gmail.com>
2018-10-06 10:36:10 -0400

    Remove unnecessary files

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-const clientESLintConfig = require('./eslint');
-
-module.exports = Object.assign({}, clientESLintConfig, {
-  env: Object.assign({}, clientESLintConfig.env, {
-    node: true
-  })
-});
